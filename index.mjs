a0bd6588c276c86558e3d0b7dffcae0480230e7b
Kamvelihle Innocent Mjanyelwa <31170359+FutureMfana@users.noreply.github.com>
2022-08-11 22:33:05 +0200

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -3,29 +3,46 @@ import * as backend from './build/index.main.mjs';
 const stdlib = loadStdlib(process.env);
 
 const startingBalance = stdlib.parseCurrency(100);
-const accNeedy = await stdlib.newTestAccount(stdlib.parseCurrency(0));
+const accNeedy = await stdlib.newTestAccount(stdlib.parseCurrency(5));
 const accDonor = await stdlib.newTestAccount(startingBalance);
 const accVerifier = await stdlib.newTestAccount(startingBalance);
 
+console.log('Starting application...');
+
 const ctcNeedy = accNeedy.contract(backend);
+
+/*
+await ctcNeedy.participants.Needy({
+    DonationNeeded: stdlib.parseCurrency(15),
+    DonationCause: 'Needed for Doctor\'s operation',
+});*/
+
 const ctcVerifier = accVerifier.contract(backend, ctcNeedy.getInfo());
+
+/*await ctcVerifier.participants.Verifier({
+    ApproveDonation: () => {
+        console.log('Do you wish to approve this donation request?');
+        return true;
+    }
+});
+
 const ctcDonor = accDonor.contract(backend, ctcVerifier.getInfo());
 
 const RequestApproval = (Who) => ({
-    
-});
+
+});*/
 
 await Promise.all([
     ctcNeedy.p.Needy ({
-        DonationNeeded: stdlib.parseCurrency(10), 
+        DonationNeeded: stdlib.parseCurrency(10),
         DonationCause: 'For doctors operation',
-        WalletID: accNeedy.networkAccount,
     }),
     ctcVerifier.p.Verifier({
-        VerifierID: accVerifier.networkAccount,
-        ApproveDonation: () => {
+        ApproveDonation: async() => {
             const IsApproved = await ask.ask(`Would you like to approve?`, ask.yesno);
             return IsApproved;
         }
     }),
 ]);
+
+ask.done();
