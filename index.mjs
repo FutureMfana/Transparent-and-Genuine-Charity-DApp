e3a77704dddc4a58c5782b115573917a15dd27bb
Kamvelihle Innocent Mjanyelwa <31170359+FutureMfana@users.noreply.github.com>
2022-08-18 21:05:46 +0200

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -7,6 +7,12 @@ const accNeedy = await stdlib.newTestAccount(stdlib.parseCurrency(5));
 const accDonor = await stdlib.newTestAccount(startingBalance);
 const accVerifier = await stdlib.newTestAccount(startingBalance);
 
+const fmt = (x) => stdlib.formatCurrency(x, 4);
+const getBalance = async (who) => fmt(await stdlib.balanceOf(who));
+const donorBal = await getBalance(accDonor); //before donations
+const needyBal = await getBalance(accNeedy);  //before donations
+
+
 console.log('Starting application...');
 
 const ctcNeedy = accNeedy.contract(backend);
@@ -24,25 +30,31 @@ const ctcVerifier = accVerifier.contract(backend, ctcNeedy.getInfo());
         console.log('Do you wish to approve this donation request?');
         return true;
     }
-});
-
-const ctcDonor = accDonor.contract(backend, ctcVerifier.getInfo());
-
-const RequestApproval = (Who) => ({
-
 });*/
+
+const makeDonor = async (acc) =>{
+    const ctc = acc.contract(backend, ctcNeedy.getInfo());
+    await ctc.api.Donors.Donates(stdlib.parseCurrency(5));
+}
 
 await Promise.all([
     ctcNeedy.p.Needy ({
-        DonationNeeded: stdlib.parseCurrency(10),
+        DonationNeeded: stdlib.parseCurrency(20),
         DonationCause: 'For doctors operation',
     }),
     ctcVerifier.p.Verifier({
         ApproveDonation: async() => {
             const IsApproved = await ask.ask(`Would you like to approve?`, ask.yesno);
-            return IsApproved;
+            return true;
         }
     }),
+    makeDonor(accDonor),
 ]);
 
 ask.done();
+
+const afterDonor = await getBalance(accDonor);
+const afterNeedy = await getBalance(accNeedy);
+
+console.log(`National Government went from ${donorBal} to ${afterDonor}.`);
+console.log(`Local Municipality1 went from ${needyBal} to ${afterNeedy}.`);
