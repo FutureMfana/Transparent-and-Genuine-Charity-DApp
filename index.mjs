80327357e130fe0b9f4a5e99dcde5767c1fa22bd
Kamvelihle Innocent Mjanyelwa <31170359+FutureMfana@users.noreply.github.com>
2022-08-19 22:00:25 +0200

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -4,57 +4,57 @@ const stdlib = loadStdlib(process.env);
 
 const startingBalance = stdlib.parseCurrency(100);
 const accNeedy = await stdlib.newTestAccount(stdlib.parseCurrency(5));
-const accDonor = await stdlib.newTestAccount(startingBalance);
 const accVerifier = await stdlib.newTestAccount(startingBalance);
 
 const fmt = (x) => stdlib.formatCurrency(x, 4);
 const getBalance = async (who) => fmt(await stdlib.balanceOf(who));
-const donorBal = await getBalance(accDonor); //before donations
-const needyBal = await getBalance(accNeedy);  //before donations
+const needyBal = await getBalance(accNeedy);  //before donations*/
+let donorBal = 0;
 
 
 console.log('Starting application...');
 
 const ctcNeedy = accNeedy.contract(backend);
 
-/*
-await ctcNeedy.participants.Needy({
-    DonationNeeded: stdlib.parseCurrency(15),
-    DonationCause: 'Needed for Doctor\'s operation',
-});*/
-
 const ctcVerifier = accVerifier.contract(backend, ctcNeedy.getInfo());
+let done = false;
 
-/*await ctcVerifier.participants.Verifier({
-    ApproveDonation: () => {
-        console.log('Do you wish to approve this donation request?');
-        return true;
+const startingDonations = async (amt) => {
+    const makeDonor = async (who) => {
+        const accDonor = await stdlib.newTestAccount(startingBalance);
+        const amount = stdlib.parseCurrency(Math.random() * amt);
+        donorBal = await getBalance(accDonor); //before donations
+        const ctc = accDonor.contract(backend, ctcNeedy.getInfo());
+        try {
+            await ctc.apis.Donors.Donates(amount);
+            console.log(`${who} has donated ${amount}`);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    await makeDonor('Alice');
+    await makeDonor('Bob');
+    await makeDonor('Daine');
+    
+    while ( ! done ) {
+        await stdlib.wait(1);
     }
-});*/
-
-const makeDonor = async (acc) =>{
-    const ctc = acc.contract(backend, ctcNeedy.getInfo());
-    await ctc.api.Donors.Donates(stdlib.parseCurrency(5));
-}
+};
 
 await Promise.all([
     ctcNeedy.p.Needy ({
         DonationNeeded: stdlib.parseCurrency(20),
         DonationCause: 'For doctors operation',
+        StartDonations: () => {
+            startingDonations(20);
+        },
     }),
     ctcVerifier.p.Verifier({
         ApproveDonation: async() => {
             const IsApproved = await ask.ask(`Would you like to approve?`, ask.yesno);
-            return true;
+            return IsApproved;
         }
     }),
-    makeDonor(accDonor),
 ]);
 
-ask.done();
-
-const afterDonor = await getBalance(accDonor);
-const afterNeedy = await getBalance(accNeedy);
-
-console.log(`Donor went from ${donorBal} to ${afterDonor}.`);
-console.log(`Needy went from ${needyBal} to ${afterNeedy}.`);
+done = true;
