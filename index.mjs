9c47aed6cd1815cde067d2d538e48161e0c47230
Kamvelihle Innocent Mjanyelwa <31170359+FutureMfana@users.noreply.github.com>
2022-08-23 20:21:40 +0200

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -11,7 +11,6 @@ const getBalance = async (who) => fmt(await stdlib.balanceOf(who));
 const needyBal = await getBalance(accNeedy);  //before donations*/
 let donorBal = 0;
 
-
 console.log('Starting application...');
 
 const ctcNeedy = accNeedy.contract(backend);
@@ -22,32 +21,49 @@ let done = false;
 const startingDonations = async (amt) => {
     const makeDonor = async (who) => {
         const accDonor = await stdlib.newTestAccount(startingBalance);
-        const amount = stdlib.parseCurrency(Math.random() * amt);
-        donorBal = await getBalance(accDonor); //before donations
-        const ctc = accDonor.contract(backend, ctcNeedy.getInfo());
-        try {
-            await ctc.apis.Donors.Donates(amount);
-            console.log(`${who} has donated ${amount}`);
-        } catch (error) {
-            console.log(error);
+        let randVal = Math.random() * amt; 
+        let finalNeedyBal = await getBalance(accNeedy); //balance after donations
+        
+        console.log(`Required amount is: ${amt}`);
+        console.log(`Donation amount is: ${randVal}`);
+        console.log((parseFloat(finalNeedyBal) + randVal) + '\n-----------------------------------\n\n');
+
+        if ((parseFloat(finalNeedyBal) + randVal) <= amt){
+            const amount = stdlib.parseCurrency(randVal);
+            donorBal = await getBalance(accDonor); //before donations
+            const ctc = accDonor.contract(backend, ctcNeedy.getInfo());
+            try {
+                await ctc.apis.Donors.Donates(amount);
+                console.log(`${who} has donated ${randVal}`);
+            } catch (error) {
+                console.log(error);
+            }
+        }else{
+            console.log(`You have dononated more than required!`);
         }
     };
+
     await makeDonor('Alice');
     await makeDonor('Bob');
     await makeDonor('Daine');
+    let finalNeedyBal = await getBalance(accNeedy); //balance after donations
+    console.log(`Balance is ${finalNeedyBal}`)
     
-    while ( ! done ) {
+    
+    //console.log(`\nNeedy balance from ${needyBal} to ${finalNeedyBal}`);
+    /*while ( ! done ) {
         await stdlib.wait(1);
-    }
+    }*/
 };
 
 await Promise.all([
     ctcNeedy.p.Needy ({
         DonationNeeded: stdlib.parseCurrency(20),
-        DonationCause: 'For doctors operation',
+        DonationCause: 'for doctors operation',
         StartDonations: () => {
             startingDonations(20);
         },
+        //lenInBlocks: 10,
     }),
     ctcVerifier.p.Verifier({
         ApproveDonation: async() => {
