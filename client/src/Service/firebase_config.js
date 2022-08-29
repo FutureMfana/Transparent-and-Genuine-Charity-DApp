f6853e63292e8209720f348a168559f563252554
Kamvelihle Innocent Mjanyelwa <31170359+FutureMfana@users.noreply.github.com>
2022-08-29 09:59:00 +0200

    Update firebase_config.js

--- client/src/Service/firebase_config.js
+++ client/src/Service/firebase_config.js
@@ -6,12 +6,7 @@ import {getFirestore} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDr8n4B6UfK4jm-5tZk8FJS4eMU9u9gfBY",
-    authDomain: "reach-charity.firebaseapp.com",
-    projectId: "reach-charity",
-    storageBucket: "reach-charity.appspot.com",
-    messagingSenderId: "277535621998",
-    appId: "1:277535621998:web:37bf4d978a990e4a08f77f"
+    //Your API Keys goes in here
 };
 
 // Initialize Firebase
